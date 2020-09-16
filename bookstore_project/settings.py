d38abd2cb83ce7145ec36982db25e3cd1fee69b3
premwat <premwat@hotmail.com>
2020-09-16 08:32:14 +0700

    ch5 update

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
@@ -126,3 +127,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'users.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
